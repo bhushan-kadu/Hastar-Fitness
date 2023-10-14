298d43c5e15d5837188011c012320a85df684278
bkadu3 <bkadu3@gmail.com>
2023-10-14 23:16:44 +0530

    etst

--- app/src/main/java/com/hastarfitness/hastarfitnessapp/AboutUs.kt
+++ app/src/main/java/com/hastarfitness/hastarfitnessapp/AboutUs.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_about_us.*
-
+//this is a comment
 class AboutUs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
